33a8c7b5884da032fdd3e4ca56c58f87664f2484
Austin Yen <38061641+austinyen56@users.noreply.github.com>
2022-07-27 22:32:24 -0700

    [UPDATE] v2.3 Plenty of updated features
    2.3?? Weird number but essentially plenty of features are being added (v2) and 3 main functionalities are implemented already (hence the .3)!
    Auto mode isnt fully fleshed out yet and the .exe arent updated, but will definitely be updated soon!
    
    Key features updated:
    - Fixed spacing, naming, and version for all print()
    - Created functions to be able to save and store results into outfile.txt and to clipboard as well
    - Created function for auto sorting (WIP), having trouble fetching enemy data

--- RitoWinconCalculator.py
+++ RitoWinconCalculator.py
@@ -1,18 +1,24 @@
 '''
-League of Legends Wincon Calculator v1.2 @austinyen56
-All stats are from the riot api, (leagueofgraphs.com/champions/), and (na.op.gg/champion/statistics)
+League of Legends Wincon Calculator v2.3 @austinyen56
+All stats are from the RiotWatcher API, LCU API, (leagueofgraphs.com/champions/), and (na.op.gg/champion/statistics)
 
 Displays checks for matchup, healing, cc, and damage type
 '''
+from typing import List
 import championdb as champion
 import os
+import pyperclip
+import sys
 
+# Initialzing variables blue1 - blue5, red1 - red5
 b1, b2, b3, b4, b5 = ["" for _ in range(5)]
 r1, r2, r3, r4, r5 = ["" for _ in range(5)]
 
-print("===============Team comp win con calculator v1.2 @austinyen56===============")
+print("=============== LoL Wincon Calculator v2.3 @austinyen56 ===============")
 print("------Please type everything in lowercase in one word---------\n")
+
 # -----------------Sorting team comp-------------
+# Checking if the player is first pick (Will know which side and therefore pick order)
 whatpick = True
 pick = ""
 pickString = ""
@@ -34,7 +40,7 @@ playerPrint = ["Enter ally 1: ", "Enter enemy 1: ", "Enter enemy 2: ", "Enter al
 champPrintB = []
 champPrintR = []
 
-
+# First Pick Order
 def fp():
     global playerPrint
     print("You are on blue side")
@@ -56,7 +62,7 @@ def fp():
     champPrintB.extend([b1, b2, b3, b4, b5])
     champPrintR.extend([r1, r2, r3, r4, r5])
 
-
+# Second Pick Order
 def sp():
     global playerPrint
     print("You are on red side")
@@ -78,7 +84,7 @@ def sp():
     champPrintB.extend([b1, b2, b3, b4, b5])
     champPrintR.extend([r1, r2, r3, r4, r5])
 
-
+# Role correction if mistyped
 def roleCorrection(x):
     if x == "middle":
         print("Type 'mid' instead\n")
@@ -89,7 +95,21 @@ def roleCorrection(x):
     if x == "jungle":
         print("Type 'jg' instead\n")
 
+# Function that ONLY prints to outfile
+def ONLYprintingtoFile(*content):       
+        originalOut = sys.stdout
+        with open('outfile.txt', 'a+') as h:
+                sys.stdout = h
+                print("".join([i for i in content]))
+                sys.stdout = originalOut
 
+# Function that prints to both outfile and console
+def printConsoleandOutfile(*content):
+    with open('outfile.txt', 'a+') as o:
+        for out in [sys.stdout, o]:
+            print("".join([i for i in content]), file=out)
+
+# Check roles for each player (Manual mode)
 def roleCheck():
     global champPrintB
     champRoleB = {}
@@ -137,23 +157,27 @@ def roleCheck():
 
     # print("Blue team: ", champRoleB) #Blue team:  {'heimerdinger': 'supp', 'gwen': 'top', 'masteryi': 'jg', 'zed': 'mid', 'ezreal': 'adc'}
     # print("Red team: ", champRoleR) #Red team:  {'sona': 'supp', 'twitch': 'adc', 'morgana': 'mid', 'gragas': 'jg', 'renekton': 'top'}
+    
+    # ======================================= Analyitics, Calculating, and Printing =============================
+    
     print("\n")
-    print("          =================Matchup=================")
+    ONLYprintingtoFile("===============Generated from LoL Wincon Calculator v2.0 @austinyen56===============")
+    printConsoleandOutfile("          =================Matchup=================")
 
     def get_champion_from_pos(val, x):
         for key, value in x.items():
             if val == value:
                 return key
 
-    print("Top lane:     ", get_champion_from_pos('top', champRoleB).ljust(20, "."), "vs".center(0),
+    printConsoleandOutfile("Top lane:     ", get_champion_from_pos('top', champRoleB).ljust(20, "."), "vs".center(0),
           get_champion_from_pos('top', champRoleR).rjust(20, "."))
-    print("Mid lane:     ", get_champion_from_pos('mid', champRoleB).ljust(20, "."), "vs".center(0),
+    printConsoleandOutfile("Mid lane:     ", get_champion_from_pos('mid', champRoleB).ljust(20, "."), "vs".center(0),
           get_champion_from_pos('mid', champRoleR).rjust(20, "."))
-    print("Jungle:       ", get_champion_from_pos('jg', champRoleB).ljust(20, "."), "vs".center(0),
+    printConsoleandOutfile("Jungle:       ", get_champion_from_pos('jg', champRoleB).ljust(20, "."), "vs".center(0),
           get_champion_from_pos('jg', champRoleR).rjust(20, "."))
-    print("Adc:          ", get_champion_from_pos('adc', champRoleB).ljust(20, "."), "vs".center(0),
+    printConsoleandOutfile("Adc:          ", get_champion_from_pos('adc', champRoleB).ljust(20, "."), "vs".center(0),
           get_champion_from_pos('adc', champRoleR).rjust(20, "."))
-    print("Supp:         ", get_champion_from_pos('supp', champRoleB).ljust(20, "."), "vs".center(0),
+    printConsoleandOutfile("Supp:         ", get_champion_from_pos('supp', champRoleB).ljust(20, "."), "vs".center(0),
           get_champion_from_pos('supp', champRoleR).rjust(20, "."))
 
     # Clearing the list and append champions in order [top, mid, jg, adc, supp]
@@ -163,17 +187,47 @@ def roleCheck():
     for r in sortroles:
         champPrintB.append(get_champion_from_pos(r, champRoleB))
         champPrintR.append(get_champion_from_pos(r, champRoleR))
+    print(champPrintB)
+    print(champPrintR)
+
+def autoRoleSort()-> List:
+    #['garen', 'rammus', 'khazix', 'sivir', 'thresh'] top mid jg adc supp
+    #['vladimir', 'twistedfate', 'volibear', 'ashe', 'senna']
+    pass
 
 
-if pick == "yes":
-    fp()
-    roleCheck()
-    print("You are on Blue Team")
+def manualorauto():
 
-if pick == "no":
-    sp()
-    roleCheck()
-    print("You are on Red Team")
+    modeBool = True
+    while modeBool:
+        mode = input("Do you want to enter champs and roles manually or automatically? (manual/auto): ")
+        if mode == "manual":
+            modeBool = False
+            if pick == "yes":
+                fp()
+                roleCheck()
+                printConsoleandOutfile("You are on Blue Team")
+
+            if pick == "no":
+                sp()
+                roleCheck()
+                printConsoleandOutfile("You are on Red Team")
+
+        elif mode == "auto":
+            modeBool = False
+            autoRoleSort()
+        else:
+            print("Not a valid input")
+            continue
+
+
+manualorauto()
+
+
+
+
+    
+    
 
 # ------------------------------Calculate winrate-----------------------------
 # start off with 50% wr on each side
@@ -189,7 +243,7 @@ BlueWR = 50
 RedWR = 50
 
 print("\n")
-print("          =================Stats=================")
+printConsoleandOutfile("          =================Stats=================")
 # print(champPrintB)
 # print(champPrintR)
 # print(champion.CHAMPION.get("aatrox"))
@@ -199,27 +253,27 @@ for i in range(5):
     ctr = 5
     if (champPrintB[i] in champion.CHAMPION.get(champPrintR[i]).get("crushes") or (
             champPrintR[i] in champion.CHAMPION.get(champPrintB[i]).get("counters"))):
-        print(champPrintB[i], "might be countered by", champPrintR[i])
+        printConsoleandOutfile(champPrintB[i], " might be countered by ", champPrintR[i])
         BlueWR -= 5
         RedWR += 5
         ctr -= 1
     # Check if enemy is being countered
     elif (champPrintB[i] in champion.CHAMPION.get(champPrintR[i]).get("counters") or (
             champPrintR[i] in champion.CHAMPION.get(champPrintB[i]).get("crushes"))):
-        print(champPrintB[i], "can probably beat", champPrintR[i])
+        printConsoleandOutfile(champPrintB[i], " can probably beat ", champPrintR[i])
         BlueWR += 5
         RedWR -= 5
         ctr -= 1
     else:
-        print(champPrintB[i], "is an even matchup to", champPrintR[i])
+        printConsoleandOutfile(champPrintB[i], " is an even matchup to ", champPrintR[i])
 
     if ctr == 0:
         print("\n")
-        print("All lanes have even matchups")
+        printConsoleandOutfile("All lanes have even matchups")
 
 print("\n")
-print("          =================Healing Check=================")
-print("          ----------------You are on", pickString, "----------")
+printConsoleandOutfile("          =================Healing Check=================")
+printConsoleandOutfile("          ----------------You are on ", pickString, "----------")
 BlueHealTotal = 0
 RedHealTotal = 0
 
@@ -229,21 +283,21 @@ for i in range(5):
 
 if abs(BlueHealTotal - RedHealTotal) >= 5:
     if BlueHealTotal > RedHealTotal:
-        print("Red team might get healing reduction items")
+        printConsoleandOutfile("Red team might get healing reduction items")
         BlueWR += 3
         RedWR -= 3
     if BlueHealTotal < RedHealTotal:
-        print("Blue team might have to get healing reduction items")
+        printConsoleandOutfile("Blue team might have to get healing reduction items")
         BlueWR -= 3
         RedWR += 3
 else:
-    print("No healing reduction items are needed for either side")
-print("Blue team heal score:", BlueHealTotal)
-print("Red team heal score:", RedHealTotal)
+    printConsoleandOutfile("No healing reduction items are needed for either side")
+printConsoleandOutfile("Blue team heal score: ", str(BlueHealTotal))
+printConsoleandOutfile("Red team heal score: ", str(RedHealTotal))
 
 print("\n")
-print("          =================CC Check=================")
-print("          ----------------You are on", pickString, "----------")
+printConsoleandOutfile("          =================CC Check=================")
+printConsoleandOutfile("          ----------------You are on ", pickString, "----------")
 BlueCCTotal = 0
 BlueKnockupTotal = 0
 RedCCTotal = 0
@@ -257,42 +311,42 @@ for i in range(5):
 err = False
 if err == False:
     if BlueCCTotal >= 15:
-        print("Blue team has alot of cc, play for teamfights")
+        printConsoleandOutfile("Blue team has alot of cc, play for teamfights")
         BlueWR += 8
         err = True
     if BlueCCTotal <= 3:
-        print("Blue team has barely any cc, be careful")
+        printConsoleandOutfile("Blue team has barely any cc, be careful")
         BlueWR -= 8
         err = True
     if BlueKnockupTotal >= 2:
-        print("Blue team can potentially make a wombo combo")
+        printConsoleandOutfile("Blue team can potentially make a wombo combo")
         BlueWR += 5
         err = True
 
     if RedCCTotal >= 15:
-        print("Red team has alot of cc, watch out and get picks")
+        printConsoleandOutfile("Red team has alot of cc, watch out and get picks")
         RedWR += 8
         err = True
     if RedCCTotal <= 3:
-        print("Red team has barely any cc, punish them")
+        printConsoleandOutfile("Red team has barely any cc, punish them")
         RedWR -= 8
         err = True
     if RedKnockupTotal >= 2:
-        print("Red team can potentially make a wombo combo")
+        printConsoleandOutfile("Red team can potentially make a wombo combo")
         RedWR += 5
         err = True
 
 if err == False:
-    print("Both teams have equal amount of cc")
+    printConsoleandOutfile("Both teams have equal amount of cc")
 
-print("Blue team CC score:", BlueCCTotal)
-print("Red team CC score:", RedCCTotal)
-print("Blue team knockup score:", BlueKnockupTotal)
-print("Red team knockup score:", RedKnockupTotal)
+printConsoleandOutfile("Blue team CC score: ", str(BlueCCTotal))
+printConsoleandOutfile("Red team CC score: ", str(RedCCTotal))
+printConsoleandOutfile("Blue team knockup score: ", str(BlueKnockupTotal))
+printConsoleandOutfile("Red team knockup score: ", str(RedKnockupTotal))
 
 print("\n")
-print("          =================Team Comp Check=================")
-print("          ----------------You are on", pickString, "----------")
+printConsoleandOutfile("          =================Team Comp Check=================")
+printConsoleandOutfile("          ----------------You are on", pickString, "----------")
 AllBlueRoles = [champion.CHAMPION.get(champPrintB[b]).get("role") for b in range(len(champPrintB))]
 AllRedRoles = [champion.CHAMPION.get(champPrintR[r]).get("role") for r in range(len(champPrintR))]
 BRFreq = {}
@@ -318,34 +372,34 @@ freq = False
 if freq == False:
     if 'tank' not in BRFreq:
         if 'fighter' not in BRFreq:
-            print("Blue team has no tanks or fighters, they are very squishy")
+            printConsoleandOutfile("Blue team has no tanks or fighters, they are very squishy")
             freq = True
             BlueWR -= 10
         elif int(BRFreq.get('fighter')) < 2:
-            print("Blue team has no tanks or not enough fighters, they are very squishy")
+            printConsoleandOutfile("Blue team has no tanks or not enough fighters, they are very squishy")
             freq = True
             BlueWR -= 10
 
     if 'tank' not in RRFreq:
         if 'fighter' not in RRFreq:
-            print("Red team has no tanks or fighters, they are very squishy")
+            printConsoleandOutfile("Red team has no tanks or fighters, they are very squishy")
             freq = True
             RedWR -= 10
         elif int(RRFreq.get('fighter')) < 2:
-            print("Red team has no tanks or enough fighters, they are very squishy")
+            printConsoleandOutfile("Red team has no tanks or enough fighters, they are very squishy")
             freq = True
             RedWR -= 10
 
 if freq == False:
-    print("Both teams have equal amount of tanks or fighters")
+    printConsoleandOutfile("Both teams have equal amount of tanks or fighters")
 
 # In future, u can check for meta or not, make list for each role consisting of each champ
 # and then check if the certain role is in the list created
 # if not meta then -3% wr?
 
 print("\n")
-print("          =================Dmg Type Check=================")
-print("          ----------------You are on", pickString, "----------")
+printConsoleandOutfile("          =================Dmg Type Check=================")
+printConsoleandOutfile("          ----------------You are on", pickString, "----------")
 AllBlueDmg = [champion.CHAMPION.get(champPrintB[b]).get("dmgtype") for b in range(len(champPrintB))]
 AllRedDmg = [champion.CHAMPION.get(champPrintR[r]).get("dmgtype") for r in range(len(champPrintR))]
 BDTotal = {}
@@ -357,53 +411,53 @@ dmgerr = False
 if dmgerr == False:
     if 'ad' in BDTotal:
         if BDTotal.get('ad') >= 4:
-            print("Blue side has too much ad")
+            printConsoleandOutfile("Blue side has too much ad")
             BlueWR -= 5
             dmgerr = True
     else:
-        print("Blue side has no ap (excluding hybrids)")
+        printConsoleandOutfile("Blue side has no ap (excluding hybrids)")
 
     if 'ap' in BDTotal:
         if BDTotal.get('ap') >= 4:
-            print("Blue side has too much ap")
+            printConsoleandOutfile("Blue side has too much ap")
             BlueWR -= 5
             dmgerr = True
     else:
-        print("Blue side has no ap (excluding hybrids)")
+        printConsoleandOutfile("Blue side has no ap (excluding hybrids)")
 
     if 'ad' in RDTotal:
         if RDTotal.get('ad') >= 4:
-            print("Red side has too much ad")
+            printConsoleandOutfile("Red side has too much ad")
             RedWR -= 5
             dmgerr = True
     else:
-        print("Red side has no ap (excluding hybrids)")
+        printConsoleandOutfile("Red side has no ap (excluding hybrids)")
 
     if 'ap' in RDTotal:
         if RDTotal.get('ap') >= 4:
-            print("Red side has too much ap")
+            printConsoleandOutfile("Red side has too much ap")
             RedWR -= 5
             dmgerr = True
     else:
-        print("Red side has no ap (excluding hybrids)")
+        printConsoleandOutfile("Red side has no ap (excluding hybrids)")
 
 if dmgerr == False:
-    print("Both sides have equal amount of ap and ad")
+    printConsoleandOutfile("Both sides have equal amount of ap and ad")
 
 print("\n")
-print("Blue WR:", BlueWR, "%")
-print("Red WR:", RedWR, "%")
+printConsoleandOutfile("Blue WR: ", str(BlueWR), "%")
+printConsoleandOutfile("Red WR: ", str(RedWR), "%")
 
 
 # Are you more likely to win or lose?
 def dodgeORnah(wr):
     if wr >= 70:
         if abs(BlueWR - RedWR) >= 18:
-            print("You have a huge advantage, win like a boss!")
+            printConsoleandOutfile("You have a huge advantage, win like a boss!")
 
     if wr <= 43:
         if abs(BlueWR - RedWR) >= 18:
-            print("You have a HUGE disadvantage, dodge like the plague")
+            printConsoleandOutfile("You have a HUGE disadvantage, dodge like the plague")
             byeee = input("Do you want to dodge to save LP? ")
             if byeee == 'yes':
                 print("Executing Order 66 on league client to save LP...")
@@ -415,20 +469,34 @@ def dodgeORnah(wr):
 
 if BlueWR > RedWR:
     if pickString == 'blue side':
-        print("You are more likely to win")
+        printConsoleandOutfile("You are more likely to win")
         dodgeORnah(BlueWR)
     if pickString == 'red side':
-        print("You are more likely to lose")
+        printConsoleandOutfile("You are more likely to lose")
         dodgeORnah(RedWR)
 elif BlueWR < RedWR:
     if pickString == 'blue side':
-        print("You are more likely to lose")
+        printConsoleandOutfile("You are more likely to lose")
         dodgeORnah(BlueWR)
     if pickString == 'red side':
-        print("You are more likely to win")
+        printConsoleandOutfile("You are more likely to win")
         dodgeORnah(RedWR)
 else:
-    print("Both sides are equal in win rates, better player wins!")
+    printConsoleandOutfile("Both sides are equal in win rates, better player wins!")
 
 print("\n")
+
+def copytoClipboard():
+    result = ""
+    with open('outfile.txt', 'r') as r:
+
+        result = r.read()
+    #print(result)
+    
+    pyperclip.copy(result)
+
+if input("Do you want to copy to clipboard? (y/n): ") == "y":
+    copytoClipboard()
+    print("Copied to clipboard!")
+
 input("Click the enter key to exit...")
